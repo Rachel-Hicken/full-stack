6a0997ddde469a8d2ef20f9f7797ba3872abb4a9
Rachel Hicken <rachkh22@gmail.com>
2018-05-17 13:57:28 -0600

    starting front end

---
+++ instructor-code/src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <div>
+            <img src="" alt=""/>
+            <a href={process.env.REACT_APP_LOGIN}>
+                <button>Login</button>
+            </a>
+        </div> 
+    )
+}
